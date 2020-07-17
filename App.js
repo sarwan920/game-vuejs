6b99519ee6192bd662c9650bf59ea586ef1cdcec
Sarwan Nizamani <sarwan.nizamani8@gmail.com>
2020-07-18 01:59:30 +0500

    add attack functionality

--- App.js
+++ App.js
@@ -6,31 +6,56 @@ new Vue({
         gameIsRunning:false
     },
     methods: {
+
+        //this fucntion executes when we click on start game button
         startGame: function() {
             this.gameIsRunning=true;
             this.playerHealth=100;
             this.monsterHealth=100;
         },
-        attack:function(){
-            let min=1,max=100;
-             
-            var random=Math.random() * (max - min) + min;
-            this.playerHealth=Math.floor(random);
-            this.monsterHealth=Math.floor(random);
 
-            if(this.playerHealth <10){
-                alert("You lost and Monster Win");
+
+        //function to attack normally
+        attack:function(){
+
+            // console.log("attacked");
+            
+            var max=10;
+            var min =3;
+
+            var damage= Math.max(Math.floor(Math.random() * max) +1,min) ;
+            this.monsterHealth =this.monsterHealth-damage;
+            
+            if(this.monsterHealth<=0){
+                alert("You won!");
                 this.gameIsRunning=false;
-                this.playerHealth=100;
-                this.monsterHealth=100;
+                return;
             }
-            if(this.monsterHealth<15){
-                alert("You Won and Monster Lost");
+
+
+            max=12;
+            min=5;
+            damage= Math.max(Math.floor(Math.random() * max) +1,min) ;
+            this.playerHealth=this.playerHealth-damage;
+
+            if(this.playerHealth<=0){
+                alert('You lost!');
                 this.gameIsRunning=false;
-                this.monsterHealth=100;
-                this.playerHealth=100;
             }
+             
+            
         },
+
+        //function to use some special power to use some special attack
+        specialAttack:function(){
+
+        },
+        //function to heal your self
+        heal:function(){
+
+        },
+
+        //function for giving up on game
         giveUp: function(){
             this.gameIsRunning=false;
             this.monsterHealth=100;
